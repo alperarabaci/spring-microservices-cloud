2018a6f8501b40c1d6f5bfdbe288973d26ec1a9f
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-14 22:56:50 +0300

    "Section 20: Service Registration with Eureka" Done

---
+++ mssc-beer-inventory-service/src/main/java/app/wordyourself/beerinventoryservice/config/LocalDiscovery.java
@@ -0,0 +1,14 @@
+package app.wordyourself.beerinventoryservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * alper - 14/08/2020
+ */
+@Profile("local-discovery")
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+}
--- mssc-beer-service/src/main/java/app/wordyourself/msscbeerservice/BeerServiceApplication.java
+++ mssc-beer-service/src/main/java/app/wordyourself/msscbeerservice/BeerServiceApplication.java
@@ -2,16 +2,17 @@ package app.wordyourself.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 /**
  * -Dspring.profiles.active=localmysql
+ * -Dspring.profiles.active=local-discovery
+ *
  */
 @SpringBootApplication
-public class MsscBeerServiceApplication {
+public class BeerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsscBeerServiceApplication.class, args);
+		SpringApplication.run(BeerServiceApplication.class, args);
 	}
 
 }
