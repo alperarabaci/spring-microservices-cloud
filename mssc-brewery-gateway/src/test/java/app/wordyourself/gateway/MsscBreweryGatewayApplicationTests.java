177b0a59f979f2fc1366cf212b47e5be558d2d8b
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-14 15:49:05 +0300

    253. Spring Cloud Gateway Service Creation

---
+++ mssc-brewery-gateway/src/test/java/app/wordyourself/gateway/MsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package app.wordyourself.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
