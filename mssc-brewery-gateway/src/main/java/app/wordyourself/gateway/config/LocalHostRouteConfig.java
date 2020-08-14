874ad8ca1a25c5cdf13039797e7b31e97d0afcf7
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-14 21:42:21 +0300

    Eureka! 260. Eureka Service Creation

--- mssc-brewery-gateway/src/main/java/app/wordyourself/gateway/config/LocalHostRouteConfig.java
+++ mssc-brewery-gateway/src/main/java/app/wordyourself/gateway/config/LocalHostRouteConfig.java
@@ -11,22 +11,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class LocalHostRouteConfig {
 
-    @Bean
-    public RouteLocator localhostRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r.path("/api/v1/beer*",
-                                             "/api/v1/beer/*",
-                                             "/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080")
-                        .id("beer-service"))
-                .route(r -> r.path("/api/v1/customers*",
-                                            "/api/v1/customers/**")
-                        .uri("http://localhost:8081")
-                        .id("beer-order-service"))
-                .route(r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082")
-                        .id("beer-inventory-service"))
-                .build();
-    }
+@Bean
+public RouteLocator localhostRoutes(RouteLocatorBuilder builder) {
+    return builder.routes()
+            .route(r -> r.path("/api/v1/beer*",
+                                         "/api/v1/beer/*",
+                                         "/api/v1/beerUpc/*")
+                    .uri("http://localhost:8080")
+                    .id("beer-service"))
+            .route(r -> r.path("/api/v1/customers*",
+                                        "/api/v1/customers/**")
+                    .uri("http://localhost:8081")
+                    .id("beer-order-service"))
+            .route(r -> r.path("/api/v1/beer/*/inventory")
+                    .uri("http://localhost:8082")
+                    .id("beer-inventory-service"))
+            .build();
+}
 
 }
