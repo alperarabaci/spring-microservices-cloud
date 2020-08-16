988ca35a4a41f23d26f84c6f4b5a450e8a2d4431
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-16 22:49:18 +0300

    Config için de sunucu açtık. Zipkin için de. Su yakıyor sanki bu sunucular!

--- mssc-brewery-gateway/src/main/java/app/wordyourself/gateway/GatewayApplication.java
+++ mssc-brewery-gateway/src/main/java/app/wordyourself/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * -Dspring.profiles.active=google
+ * -Dspring.profiles.active=local-discovery
  */
 @SpringBootApplication
 public class GatewayApplication {
