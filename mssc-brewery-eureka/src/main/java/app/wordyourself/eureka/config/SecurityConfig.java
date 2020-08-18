bb21820987c08f7c8061a8b6c22de73985593e89
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-18 10:29:09 +0300

    Security Section: 287. Use Spring Security to Secure Eureka Server, 286. Secure Spring Cloud Config Server, 285. Encrypt Beer Service Passwords

---
+++ mssc-brewery-eureka/src/main/java/app/wordyourself/eureka/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package app.wordyourself.eureka.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * alper - 17/08/2020
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
