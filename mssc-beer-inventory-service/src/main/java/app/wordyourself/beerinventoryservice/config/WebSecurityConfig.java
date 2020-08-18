6253527b0a19307fa514391eaea5d3c644e1f553
Alper Arabacı <melihalper.arabaci@gmail.com>
2020-08-18 10:49:19 +0300

    288. Secure Inventory Service with Spring Security

---
+++ mssc-beer-inventory-service/src/main/java/app/wordyourself/beerinventoryservice/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package app.wordyourself.beerinventoryservice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * alper - 18/08/2020
+ */
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
